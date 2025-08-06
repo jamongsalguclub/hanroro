44c44246ecb5eee12cc18300469c54245d012ff4
wookhyung <qpflapffhs76@gmail.com>
2025-08-06 22:15:40 +0900

    feat: add initial setup with dependencies and layout

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import FuzzyText from './ui/fuzzy-text';
+
+export default function Home() {
+  return (
+    <div className='flex h-screen w-screen items-center justify-center flex-col gap-4'>
+      <FuzzyText>한로로</FuzzyText>
+      <FuzzyText>HANRORO</FuzzyText>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,49 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
-
 export const metadata: Metadata = {
-  title: 'HANRORO',
-  description: 'HANRORO',
+  title: '한로로, HANRORO',
+  description: '한로로와 관련된 다양한 정보를 제공해요.',
+  openGraph: {
+    title: '한로로, HANRORO',
+    description: '한로로와 관련된 다양한 정보를 제공해요.',
+    siteName: '한로로, HANRORO',
+    url: 'https://hanroro.com',
+    locale: 'ko_KR',
+    images: [
+      {
+        url: '/og-image.webp',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '한로로, HANRORO',
+    description: '한로로와 관련된 다양한 정보를 제공해요.',
+    images: ['/og-image.webp'],
+  },
+  authors: [{ name: '한로로', url: 'https://hanroro.com' }],
+  keywords: ['한로로', 'HANRORO', '이상비행', '집', '자몽살구클럽'],
+  icons: {
+    icon: '/favicon.ico',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  alternates: {
+    canonical: 'https://hanroro.com',
+  },
+  other: {
+    'naver-site-verification': '99eb1cf3ad2eae187fa4b3c0c3b694c5a1c3bbd9',
+  },
 };
 
 export default function RootLayout({
@@ -23,8 +52,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang='ko'>
+      <body className='antialiased'>
+        {children}
+        <GoogleAnalytics gaId='G-XYP3PBZFC8' />
+      </body>
     </html>
   );
 }
